e7b2efddce7226522df3f39680437b0a6ed669f7
Ali Feras <ferasali876@gmail.com>
2022-08-05 01:21:08 -0500

    Initial Commit

---
+++ PZa Hunter/PZa_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PZa_HunterApp.swift
+//  PZa Hunter
+//
+//  Created by Ali on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PZa_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
